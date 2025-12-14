0506a2864391520edd9c4b97c13580f4e88d7614
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-14 15:18:40 +0700

    Update ingest.py

--- steps/ingest.py
+++ steps/ingest.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import yaml
+from pathlib import Path
 
 class Ingestion:
-    def __init__(self):
+    def __init__(self, config_path="config.yml"):
+        self.config_path = Path(config_path)
         self.config = self.load_config()
 
     def load_config(self):
-        with open("config.yml", "r") as file:
+        with open(self.config_path, "r") as file:
             return yaml.safe_load(file)
 
     def load_data(self):
         train_data_path = self.config['data']['train_path']
         test_data_path = self.config['data']['test_path']
+
         train_data = pd.read_csv(train_data_path)
         test_data = pd.read_csv(test_data_path)
+
         return train_data, test_data
