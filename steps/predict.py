d1ee0dff9c24006691936b56921c31598708faec
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-14 15:33:36 +0700

    Update predict.py

--- steps/predict.py
+++ steps/predict.py
@@ -1,14 +1,15 @@
 import os
 import joblib
+import yaml
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 
 class Predictor:
     def __init__(self):
-        self.model_path = self.load_config()['model']['store_path']
+        self.config = self.load_config()
+        self.model_path = self.config['model']['store_path']
         self.pipeline = self.load_model()
 
     def load_config(self):
-        import yaml
         with open('config.yml', 'r') as config_file:
             return yaml.safe_load(config_file)
         
@@ -23,7 +24,10 @@ class Predictor:
 
     def evaluate_model(self, X_test, y_test):
         y_pred = self.pipeline.predict(X_test)
+        y_prob = self.pipeline.predict_proba(X_test)[:, 1]
+
         accuracy = accuracy_score(y_test, y_pred)
         class_report = classification_report(y_test, y_pred)
-        roc_auc = roc_auc_score(y_test, y_pred)
+        roc_auc = roc_auc_score(y_test, y_prob)
+
         return accuracy, class_report, roc_auc
