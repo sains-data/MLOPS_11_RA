2210361a68a575a3dd0b28349c2108d68ff6d83f
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-18 09:43:29 +0700

    Update train.py

--- steps/train.py
+++ steps/train.py
@@ -8,30 +8,33 @@ class Trainer:
     def __init__(self, config_path="config.yml"):
         self.config_path = config_path
         self.config = self.load_config()
-        self.model_path = self.config['model']['store_path']
-        self.model_params = self.config['model']['params']
+        self.model_path = self.config["model"]["store_path"]
+        self.model_params = self.config["model"]["params"]
         self.pipeline = self.create_pipeline()
+        self.columns = None
 
     def load_config(self):
-        with open(self.config_path, 'r') as file:
+        with open(self.config_path, "r") as file:
             return yaml.safe_load(file)
 
     def create_pipeline(self):
         model = RandomForestClassifier(**self.model_params)
-
-        pipeline = Pipeline([
+        return Pipeline([
             ("model", model)
         ])
-        return pipeline
 
     def train_model(self, X_train, y_train):
+        self.columns = X_train.columns.tolist()
         self.pipeline.fit(X_train, y_train)
 
     def save_model(self):
         os.makedirs(self.model_path, exist_ok=True)
         model_file = os.path.join(self.model_path, "model.pkl")
 
-        joblib.dump({
-            "model": self.pipeline,
-            "columns": X_train.columns.tolist()
-        }, model_file)
+        joblib.dump(
+            {
+                "model": self.pipeline,
+                "columns": self.columns
+            },
+            model_file
+        )
