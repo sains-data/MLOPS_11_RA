00c60624870164696fbf76419031b4db0ebdc84a
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-18 08:20:11 +0700

    Update train.py

--- steps/train.py
+++ steps/train.py
@@ -35,4 +35,9 @@ class Trainer:
     def save_model(self):
         os.makedirs(self.model_path, exist_ok=True)
         model_file = os.path.join(self.model_path, "model.pkl")
-        joblib.dump(self.pipeline, model_file)
+
+        joblib.dump({
+            "model": self.pipeline,
+            "columns": self.columns
+        }, model_file)
+
