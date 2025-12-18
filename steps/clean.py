b2976beb0c07e1b1587c064833059cef14090a50
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-18 09:48:48 +0700

    Update clean.py

--- steps/clean.py
+++ steps/clean.py
@@ -13,19 +13,16 @@ class Cleaner:
 
     def clean_data(self, data: pd.DataFrame):
         data = data.replace("?", np.nan)
-
+    
         y = data["class"]
         X = data.drop(columns=["class"])
-
+    
         y_encoded = self.target_encoder.fit_transform(y)
-
-        if not self.fitted:
-            X_encoded = self.feature_encoder.fit_transform(X)
-            self.fitted = True
-        else:
-            X_encoded = self.feature_encoder.transform(X)
-
+    
+        X_encoded = self.feature_encoder.fit_transform(X)
+    
         feature_names = self.feature_encoder.get_feature_names_out(X.columns)
         X_encoded = pd.DataFrame(X_encoded, columns=feature_names)
-
+    
         return X_encoded, y_encoded
+
