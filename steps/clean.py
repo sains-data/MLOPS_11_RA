111a1dca94b5eddc40bb7d6866c275085d5dff7d
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-18 09:11:43 +0700

    Update clean.py

--- steps/clean.py
+++ steps/clean.py
@@ -5,11 +5,7 @@ class Cleaner:
     def __init__(self):
         self.imputer = SimpleImputer(strategy="most_frequent")
 
-    def clean_data(self, data):
-        # Replace '?' with NaN
-        data = data.replace("?", np.nan)
-
-        # Impute missing values
-        data.iloc[:, :] = self.imputer.fit_transform(data)
-
-        return data
+    def clean_data(self, X):
+        X = X.replace("?", np.nan)
+        X.iloc[:, :] = self.imputer.fit_transform(X)
+        return X
