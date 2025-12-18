ebcf71c86349d1fe710e5eb10215a8308e5932d4
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-18 09:46:24 +0700

    Update clean.py

--- steps/clean.py
+++ steps/clean.py
@@ -4,22 +4,21 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 class Cleaner:
     def __init__(self):
-        self.feature_encoder = OneHotEncoder(handle_unknown="ignore", sparse=False)
+        self.feature_encoder = OneHotEncoder(
+            handle_unknown="ignore",
+            sparse_output=False
+        )
         self.target_encoder = LabelEncoder()
         self.fitted = False
 
     def clean_data(self, data: pd.DataFrame):
-        # Replace missing symbol
         data = data.replace("?", np.nan)
 
-        # Target
         y = data["class"]
         X = data.drop(columns=["class"])
 
-        # Encode target
         y_encoded = self.target_encoder.fit_transform(y)
 
-        # Encode features
         if not self.fitted:
             X_encoded = self.feature_encoder.fit_transform(X)
             self.fitted = True
@@ -27,7 +26,6 @@ class Cleaner:
             X_encoded = self.feature_encoder.transform(X)
 
         feature_names = self.feature_encoder.get_feature_names_out(X.columns)
-
         X_encoded = pd.DataFrame(X_encoded, columns=feature_names)
 
         return X_encoded, y_encoded
