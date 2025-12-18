be5404108c6754dc8e552fd259159f8ec0703543
Rian-Bintang <rian.122450094@student.itera.ac.id>
2025-12-18 09:15:07 +0700

    Update predict.py

--- steps/predict.py
+++ steps/predict.py
@@ -7,7 +7,7 @@ class Predictor:
     def __init__(self):
         self.config = self.load_config()
         self.model_path = self.config['model']['store_path']
-        self.pipeline = self.load_model()
+        self.model, self.columns = self.load_model()
 
     def load_config(self):
         with open('config.yml', 'r') as config_file:
@@ -15,19 +15,26 @@ class Predictor:
         
     def load_model(self):
         model_file_path = os.path.join(self.model_path, 'model.pkl')
-        return joblib.load(model_file_path)
+        artifact = joblib.load(model_file_path)
+        return artifact["model"], artifact["columns"]
 
+    def align_features(self, X):
+        return X[self.columns]
+    
     def feature_target_separator(self, data):
         X = data.iloc[:, :-1]
         y = data.iloc[:, -1]
         return X, y
 
     def evaluate_model(self, X_test, y_test):
-        y_pred = self.pipeline.predict(X_test)
-        y_prob = self.pipeline.predict_proba(X_test)[:, 1]
-
+        X_test = self.align_features(X_test)
+    
+        y_pred = self.model.predict(X_test)
+        y_prob = self.model.predict_proba(X_test)[:, 1]
+    
         accuracy = accuracy_score(y_test, y_pred)
         class_report = classification_report(y_test, y_pred)
         roc_auc = roc_auc_score(y_test, y_prob)
-
+    
         return accuracy, class_report, roc_auc
+
